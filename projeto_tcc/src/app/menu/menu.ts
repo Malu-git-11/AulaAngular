9cf0af0fc0759fe20f54cb92cbba4c916dfb2790
Malu-git-11 <malumendesrodrigues13@gmail.com>
2025-10-13 20:39:18 -0300

    Projeto de tcc usando Angular, versão corrigida.

---
+++ projeto_tcc/src/app/menu/menu.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  imports: [],
+  templateUrl: './menu.html',
+  styleUrl: './menu.css'
+})
+export class Menu {
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.getElementById('hamburger');
+  const menu = document.getElementById('menu');
+  if (hamburger && menu) {
+    hamburger.addEventListener('click', function() {
+      menu.classList.toggle('active');
+    });
+  }
+});
+
